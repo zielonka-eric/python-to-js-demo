ed59810f44b44591ada57267dfcbba40c54e31f8
Eric Zielonka <zielonka.eric@gmail.com>
2018-01-12 19:18:15 -0500

    adds d3 chart generated from python data

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from random import randint
 import webbrowser
 from flask import Flask, render_template
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 length = randint(6,12)
 some_list = []
-for i in range(length):
+for i in xrange(length):
     some_list.append(randint(50, 550))
 
-webbrowser.open('http://127.0.0.1:5000/ ')
+#webbrowser.open('http://127.0.0.1:5000/')
 
 @app.route('/')
 def index():
