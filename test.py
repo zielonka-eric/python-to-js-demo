764fd34814f68a9620199aad3e850b91a3f979dd
Eric Zielonka <zielonka.eric@gmail.com>
2018-01-12 19:54:19 -0500

    puts CSS into separate file, general clean up

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ some_list = []
 for i in xrange(length):
     some_list.append(randint(50, 550))
 
-#webbrowser.open('http://127.0.0.1:5000/')
+webbrowser.open('http://127.0.0.1:5000/')
 
 @app.route('/')
 def index():
